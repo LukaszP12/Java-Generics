2667210be5b8fd30a69af9a856a9c03efee78644
Lukasz Piwowarski <lukaszp4@onet.eu>
2024-11-25 12:06:12 +0100

    further examples

--- src/main/java/com/baeldung/GenericsDemo15.java
+++ src/main/java/com/baeldung/GenericsDemo15.java
@@ -5,6 +5,12 @@ import java.util.Optional;
 import static com.baeldung.GenericsDemo14.*;
 
 class GenericsDemo15 {
+
+    public static void handleUser(User u) {
+        String name = u.name();
+        System.out.println(name);
+    }
+
     public static void main(String[] args) {
         Optional<User> oUser = Users.getUser("Lucas");
         oUser.ifPresent(user -> {
@@ -14,5 +20,8 @@ class GenericsDemo15 {
 
         User user = oUser.orElseGet(() -> new User("123", "dummyUser", true));
         System.out.println("user.name(): " + user.name());
+
+        Optional<User> oUser1 = Users.getUser("Lucas");
+        oUser1.ifPresent(GenericsDemo15::handleUser);
     }
 }
